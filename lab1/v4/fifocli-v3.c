9e720d253c73a5b2c9f1d0362d0d192d5c566fbf
Pedro Abreu <abreu223@hotmail.com>
2019-09-14 16:36:15 -0400

    Finish assignment 1

--- lab1/v4/fifocli-v3.c
+++ lab1/v4/fifocli-v3.c
@@ -14,15 +14,12 @@
 
 #define max_size 999999
 
-volatile sig_atomic_t alarm_flag = false;
 char *server_fifo = "server_queue";
 char *client_fifo = "client_queue";
-int tries;
 char buf[max_size];
 int fd;
 
 static jmp_buf env_alarm;
-pid_t k;
 
 void handle_alarm(int sig){
   static int sigcount;
@@ -36,8 +33,8 @@ void handle_alarm(int sig){
 
 void create_client_fifo(){
   if(mkfifo(client_fifo, 0666) != 0){
-    // printf("[Client]: FIFO failed to be created\n");
-    // exit(1);
+    printf("[Client]: FIFO failed to be created\n");
+    exit(1);
   }
 }
 
@@ -50,8 +47,6 @@ char* read_command(){
   int len = strlen(buf);
 
   buf[len-1] = '\0';
-  // printf("[Client]: Writing %s,", buf);
-  // fflush(stdout);
 
 }
 
@@ -59,8 +54,6 @@ int send_request(){
   int f;
   int len = strlen(buf);
 
-  // printf("[Thread %d]: Sending '%s'\n", k, buf);
-  // fflush(stdout);
   // Open Server FIFO
   f = open(server_fifo, O_WRONLY);
 
@@ -68,34 +61,22 @@ int send_request(){
   write(f, buf, len + 1);
 
   close(f);
-  // printf("[Thread %d]: Sent!\n", k);
-  // fflush(stdout);
 }
 
 void open_read_file(){
   // Open client fifo to wait for server response
-  // printf("Open Read File\n");
-  // fflush(stdout);
   fd = open(client_fifo, O_RDONLY);
-  // printf("File Read Opened\n");
-  // fflush(stdout);
 }
 
-// Returns 1 if data was read and < 1 otherwise
 int get_response(){
   int len;
   char c;
   int i;
 
 
-  // FD_ZERO(&set);
-  // FD_SET(fd, &set);
-
   i = 0;
   c = 's';
   // Receives the response from the server
-  // Waits until the server sends an end-of-transmission character so we know the transmission is over
-  // printf("Waiting for response\n");
   while(c != 3 && i < max_size - 2){
       // read( filedesc, buff, len ); /* there was data to read */
       read(fd, &c, 1);
@@ -113,24 +94,12 @@ int get_response(){
   close(fd);
 }
 
-/*
-void request_loop(){
-  if(tries > 2){
-
-  tries++;
-  alarm(2);
-  send_request();
-
-}
-*/
-
 int main(void)
 {
-  tries = 0;
-  int rc;
   int status;
-  sigset_t mask; //, omask;
+  sigset_t mask;
 
+  // Setup non blocking signal handling
   sigemptyset(&mask);
   sigaddset(&mask, SIGALRM);
 
@@ -145,20 +114,19 @@ int main(void)
   }
 
 
-  tries = 0;
-
+  // We're ready to setup the communication with the server
   create_client_fifo();
   read_command();
 
   if (sigsetjmp(env_alarm, 1) > 2){ 
-    printf("[Thread %d]: Tried to contact the server too much, dropping request\n", k);
+    printf("Tried to contact the server too much, dropping request\n");
     fflush(stdout);
     return 0;
   }
 
   alarm(2);
   send_request();
-  open_read_file();
+  open_read_file(); // This function call hangs when no message was received
   get_response();
 
   // And we're done
