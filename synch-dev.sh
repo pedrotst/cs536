d05fc251349cc25d76a7a76b8717202cdab27607
Pedro Abreu <abreu223@hotmail.com>
2019-09-28 12:41:10 -0400

    Update synch util

--- synch-dev.sh
+++ synch-dev.sh
@@ -3,7 +3,10 @@
 # Pass as argument which server you want to connect this to
 
 umount $(pwd)/dev
-rm -rf dev
+# If unmount does not work try the following:
+## diskutil unmount force $(pwd)/dev
 
+rm -rf dev
 mkdir dev
+
 sshfs pdacost@$1.cs.purdue.edu:/homes/pdacost/my_gits/cs546/dev dev
