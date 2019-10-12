b27e60b2f4a862e7a38acc0e1d7e948f3252bcd2
Pedro Abreu <abreu223@hotmail.com>
2019-10-12 16:31:19 -0400

    {v1 Skeleton}

--- labs/lab3/v1/myfetchfile.c
+++ labs/lab3/v1/myfetchfile.c
@@ -1,161 +1,70 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <errno.h>
-#include <string.h>
 #include <netdb.h>
 
 #include <sys/types.h>
 #include <sys/socket.h>
-#include <sys/wait.h>
-#include <sys/time.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
-#define MAXBUFLEN 1500
-
-// get sockaddr, IPv4 or IPv6:
-void *get_in_addr(struct sockaddr *sa) {
-    if (sa->sa_family == AF_INET) {
-        return &(((struct sockaddr_in*)sa)->sin_addr);
-    }
-
-    return &(((struct sockaddr_in6*)sa)->sin6_addr);
-}
+#define MAXBUFSZM (1000000)
+#define STRSZ (100)
 
 /*
-$ myftp cli-ip dropwhen
-  * cli-ip is the ip I will bind
-  *
+  usage
+  $ myfetchfile filename srv-ip srv-port
  */
 int main(int argc, char *argv[]) {
-  int sockfd, acksockfd;
-  struct addrinfo;
-  struct sockaddr_storage their_addr;
-  struct sockaddr_in addr;
-  struct sockaddr ack_addr;
-  struct timeval starttime, endtime;
-  int numbytes;
-  char buf[MAXBUFLEN];
-  socklen_t addr_len;
-  char s[INET6_ADDRSTRLEN];
-  char seqno = 0;
-  socklen_t addrLen;
-  int dropwhen, dup=0, packet_count = 0, packets_dropped = 0;
-  int real_packets_count = 0;
-  int total_bytes_recv = 0;
-  int dup_bytes_recv = 0;
-  int dup_bytes = 0;
-  int fst_rec = 1;
+  char filename[STRSZ];
+  char srv_ip[16];
+  char srv_port[8];
 
-  if(argc != 3){
-    fprintf(stderr, "usage: cli-ip dropwhen\n");
+  int sockfd;
+  struct addrinfo hints, *servinfo, *p;
+  int rv, numbytes;
+
+  if(argc != 4){
+    fprintf(stderr, "usage: myfetchfile filename srv-ip srv-port\n");
     exit(1);
   }
 
-  dropwhen = atoi(argv[2]);
+  strcpy(filename, argv[1]);
+  strcpy(srv_ip, argv[2]);
+  strcpy(srv_port, argv[3]);
 
-  sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-  if(sockfd == -1){
-    printf("socket() failed\n");
+
+  memset(&hints, 0, sizeof hints);
+  hints.ai_family = AF_INET;
+  hints.ai_socktype = SOCK_DGRAM;
+
+  if ((rv = getaddrinfo(srv_ip, srv_port, &hints, &servinfo)) != 0) {
+    fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+    return 1;
+  }
+
+  p = servinfo;
+  sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol);
+
+  if(p == NULL) {
+    fprintf(stderr, "receiver: failed to create socket\n");
+    return 2;
+  }
+
+  if((numbytes = sendto(sockfd, filename, strlen(filename), 0,
+                        p->ai_addr, p->ai_addrlen)) == -1) {
+    perror("receiver: sendto");
     exit(1);
   }
 
-  addr.sin_family = AF_INET;
-  addr.sin_port =  0;
-  addr.sin_addr.s_addr = inet_addr(argv[1]); /* INADDR_ANY; */
+  freeaddrinfo(servinfo);
 
-  if (bind(sockfd, (const struct sockaddr *) &addr, sizeof(addr)) == -1) {
-    printf("bind() failed\n");
-    exit(1);
-  }
-  addrLen = sizeof addr;
-  if (getsockname(sockfd, (struct sockaddr *)&addr, &addrLen) == -1) {
-    printf("getsockname() failed\n");
-    exit(1);
-  }
-  printf("receiver: Listening at %d\n", htons(addr.sin_port));
+  printf("talker: sent %d bytes to '%s:%s'\n", numbytes, srv_ip, srv_port);
 
-  addr_len = sizeof their_addr;
-
-
-  acksockfd = socket(AF_INET, SOCK_DGRAM, 0);
-  if(acksockfd == -1){
-    printf("ack socket() failed\n");
-    exit(1);
-  }
-
-  while(seqno != 2){
-    printf("\nreceiver: Ready to receive...\n");
-    if ((numbytes = recvfrom(sockfd, buf, MAXBUFLEN-1 , 0,
-                             (struct sockaddr *)&their_addr, &addr_len)) == -1) {
-      perror("recvfrom");
-      exit(1);
-    }
-
-    if(fst_rec){
-      // What time did receiving start? We'll need that for final report
-      gettimeofday(&starttime, NULL);
-      fst_rec = 0;
-    }
-
-    // Total bytes received does not account for header
-    total_bytes_recv += numbytes - 1;
-
-    // Countes duplicate bytes received
-    if(dup){
-      dup_bytes_recv += numbytes;
-      dup = 0;
-    }
-
-    seqno = buf[0];
-
-    printf("receiver: got packet from %s\n",
-           inet_ntop(their_addr.ss_family,
-                     get_in_addr((struct sockaddr *)&their_addr),
-                     s, sizeof s));
-    buf[numbytes] = '\0';
-    printf("receiver: this is packet #%d seq: %d size: %d\n", real_packets_count+1, seqno, numbytes);
-    packet_count++;
-    real_packets_count = packet_count - packets_dropped;
-
-    printf("receiver: dropwhen: %d, packet_count: %d, real_packets_sent: %d\n", dropwhen, packet_count, real_packets_count);
-    if(dropwhen != -1 && packet_count % dropwhen == 0){
-      // Workaround to fix corner case when the last package is dropped
-      seqno = 0;
-      packets_dropped++;
-
-      // dup is a flag to point that the next byte is duplicate
-      dup = 1;
-      real_packets_count = packet_count - packets_dropped;
-      printf("receiver: package droped!!\n");
-      continue;
-    }
-
-    // We got everything alright and we acknowledge it
-    if (sendto(acksockfd, &seqno, 1, 0,
-                           (struct sockaddr*)&their_addr,
-                           sizeof their_addr) == -1) {
-      perror("receiver: sendto() failed");
-      exit(1);
-    }
-
-    printf("receiver: Ack %d sent\n", seqno);
-
-  }
-  // Cool, all went well. How much time did it take?
-  gettimeofday(&endtime, NULL);
-  double completion_time =
-    (endtime.tv_sec + (endtime.tv_usec / 1000000.0)) - (starttime.tv_sec + (starttime.tv_usec / 1000000.0));
-
-  printf("receiver: End of transmission received, tearing down communication\n");
-  printf("\nreceiver: Total bytes received:\t %d\n", total_bytes_recv);
-  printf("receiver: Duplicate Bytes:\t %d\n", dup_bytes_recv);
-  printf("receiver: Completion Time:\t %.4f s\n", completion_time);
-  printf("receiver: Speed:\t\t %.4f bps\n", total_bytes_recv / completion_time);
 
   close(sockfd);
-  close(acksockfd);
 
   return 0;
 }
--- labs/lab3/v1/myfetchfiled.c
+++ labs/lab3/v1/myfetchfiled.c
@@ -1,226 +1,88 @@
-/*
-** Run The Server, get the port and then invoke this program as
-** $ gcc -o client datagramclient.c && ./client hostname port message
-** hostname will probably be data.cs.purdue.edu
-** port is fetched from the output of the server
-** and message is whatever you feel like, have fun
-*/
-
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <errno.h>
-#include <string.h>
+
 #include <sys/types.h>
 #include <sys/socket.h>
-#include <signal.h>
-#include <sys/time.h>
-#include <setjmp.h>
+#include <sys/wait.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
-#include <netdb.h>
 
-char* allocate_sendstr(int size){
-  char *s = calloc(sizeof(char), size);
-  for(int i = 0; i < size; i++){
-    s[i] = 3;
-  }
-  return s;
-}
+#define MAXBUFSZM (1000000)
 
-static jmp_buf env_alarm;
-int timeout_count = 0;
 
-void timeout_handler(int sig){
-  timeout_count++;
-  printf("sender: Timeout, resending last packet\n");
-  fflush(stdout);
+// get sockaddr, IPv4 or IPv6:
+void *get_in_addr(struct sockaddr *sa) {
+    if (sa->sa_family == AF_INET) {
+        return &(((struct sockaddr_in*)sa)->sin_addr);
+    }
 
-  siglongjmp(env_alarm, timeout_count);
+    return &(((struct sockaddr_in6*)sa)->sin6_addr);
 }
 
 /*
-$ myftpd filesize blocksize timeout cli-ip cli-port
-* filesize - total byte to be sent
-* blocksize - max of 1471
-* timeout - in ms
-* cli-port cli-ip - obvious
-*/
-
+  usage
+  $ myfetchfiled blocksize srv-port
+ */
 int main(int argc, char *argv[]) {
   int sockfd;
-  struct addrinfo hints, *servinfo, *p;
-  struct sockaddr_storage their_addr;
-  struct itimerval itime;
-  int rv;
+  struct addrinfo;
   int numbytes;
-  char *sendstr;
-  char cli_ip[16];
-  char cli_port[8];
-  int filesize, blocksize;
-  int num_sends;
-  double timeout;
-  int seqno = 0;
+  struct sockaddr_storage their_addr;
+  socklen_t addr_len;
+  char s[INET6_ADDRSTRLEN];
+  struct sockaddr_in addr;
+  socklen_t addrLen;
+  int srv_port, blocksize;
+  char filename[MAXBUFSZM];
 
-  if (argc != 6) {
-    fprintf(stderr,"usage: filesize blocksize timeout cli-ip cli-port\n");
+  if(argc != 3){
+    fprintf(stderr, "usage: myfetchfiled blocksize srv-port");
     exit(1);
   }
 
-  filesize = atoi(argv[1]);
-  blocksize = atoi(argv[2]);
-  timeout = atof(argv[3]);
-  printf("sender: filesize %d\n", filesize);
-  printf("sender: blocksize %d\n", blocksize);
-  printf("sender: timeout %f\n", timeout);
-  /* printf("sender: timeout %f\n", (int) (timeout - ((int) timeout / 1000))); */
+  blocksize = atoi(argv[1]);
+  srv_port = atoi(argv[2]);
 
-  if(blocksize > 1471){
-    fprintf(stderr, "sender: blocksize must be smaller than 1471");
-    exit(1);
-  }
-  strcpy(cli_ip, argv[4]);
-  strcpy(cli_port, argv[5]);
-  num_sends = filesize / blocksize + (filesize % blocksize != 0);
-  printf("sender: starting connection with %s:%s\n", cli_ip, cli_port);
-  fflush(stdout);
-  sendstr = allocate_sendstr(filesize);
-
-  // Setup itimer
-  if (signal(SIGALRM, timeout_handler) == SIG_ERR) {
-    perror("sender: unable to catch SIGALRM");
+  sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+  if(sockfd == -1){
+    fprintf(stderr, "Failed to create socket\n");
     exit(1);
   }
 
-  itime.it_value.tv_sec = timeout / 1000;
-  itime.it_value.tv_usec = (timeout - itime.it_value.tv_sec) * 1000;
-  itime.it_interval = itime.it_value;
+  addr.sin_family = AF_INET;
+  addr.sin_port = htons(srv_port);
+  addr.sin_addr.s_addr = INADDR_ANY;
 
-  /* printf("tv_usec: %ld\n", itime.it_value.tv_sec); */
-  /* printf("tv_usec: %ld\n", itime.it_value.tv_usec); */
-
-  if (setitimer(ITIMER_REAL, &itime, NULL) == -1) {
-    perror("sender: error calling setitimer()");
+  if(bind(sockfd, (const struct sockaddr *) &addr, sizeof(addr)) == -1){
+    fprintf(stderr, "Failed to bind\n");
     exit(1);
   }
 
-  memset(&hints, 0, sizeof hints);
-  hints.ai_family = AF_INET;
-  hints.ai_socktype = SOCK_DGRAM;
-
-  if ((rv = getaddrinfo(cli_ip, cli_port, &hints, &servinfo)) != 0) {
-    fprintf(stderr, "sender: failed getaddrinfo(): %s\n", gai_strerror(rv));
-    return 1;
-  }
-
-  p = servinfo;
-  sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol);
-
-  if (p == NULL) {
-    fprintf(stderr, "sender: socket() failed\n");
-    return 2;
-  }
-
-  // printf("sender: Sending the following file:\n %s\n", sendstr);
-  printf("sender: Sending file of size %d bytes in %d packets\n", filesize, num_sends+1);
-  fflush(stdout);
-
-  int addr_len = sizeof their_addr;
-  char *buffer;
-  buffer = calloc(sizeof(char), blocksize+1);
-  char buf;
-
-  for(int i = 0; i < num_sends; i++){
-    // Puts sequence number at the head of the buffer
-    buffer[0] = seqno;
-
-    // Adjust last block size according the remaining bytes
-    if(i == num_sends - 1 && (filesize % blocksize != 0))
-      blocksize = filesize % blocksize;
-
-    // Slice the correct block size of the string to be sent
-    strncpy(&buffer[1], &sendstr[blocksize*i], blocksize);
-    // buffer[blocksize] = '\0';
-
-    // If alarm goes off we restart the communication from here
-    if (sigsetjmp(env_alarm, 1) > 2){
-      printf("sender: tried to contact the server too much, dropping request\n");
-      fflush(stdout);
-      return 0;
-    }
-
-    // All set, we can send the packet now
-    printf("\nsender: sending %d bytes seq %d\n", blocksize, buffer[0]);
-    // printf("\nsender: sending seq %d'%s\n", buffer[0], &buffer[1]);
-    if ((numbytes = sendto(sockfd, buffer, blocksize+1, 0,
-                           p->ai_addr, p->ai_addrlen)) == -1) {
-      perror("sender: sendto() failed");
-      exit(1);
-    }
-    printf("sender: sent %d/%d packets to '%s:%s'\n", i+1, num_sends, cli_ip, cli_port);
-    printf("sender: waiting for ACK...\n");
-    if ((numbytes = recvfrom(sockfd, &buf, 1 , 0,
-                             (struct sockaddr *)&their_addr, &addr_len)) == -1) {
-      perror("recvfrom");
-      exit(1);
-    }
-
-    // Communication went well, reset timeout
-    if (setitimer(ITIMER_REAL, &itime, NULL) == -1) {
-      perror("sender: error calling setitimer()");
-      exit(1);
-    }
-    timeout_count = 0;
-
-    printf("sender: received ACK:%c\n", buf + '0');
-
-    seqno = (seqno+1)%2;
-  }
-
-  // Sweet, all packages dully sent.
-  // Let's send the end of transmission message now.
-  // We retry twice by the way
-  if (sigsetjmp(env_alarm, 1) > 2){
-    printf("sender: tried to contact the server too much, dropping request\n");
-    printf("sender: Transfer was successful anyways though\n");
-    printf("sender: Tearing down the server\n");
-    printf("sender: Good night\n");
-    fflush(stdout);
-    return 0;
-  }
-
-  printf("\nsender: Sending end of transmission\n");
-  buffer[0] = 2;
-  if ((numbytes = sendto(sockfd, buffer, 1, 0,
-                         p->ai_addr, p->ai_addrlen)) == -1) {
-    perror("sender: sendto() failed");
+  addrLen = sizeof addr;
+  if(getsockname(sockfd, (struct sockaddr *)&addr, &addrLen) == -1){
+    fprintf(stderr, "getsockname() failed\n");
     exit(1);
   }
 
-  if ((numbytes = recvfrom(sockfd, &buf, 1 , 0,
-                           (struct sockaddr *)&their_addr, &addr_len)) == -1) {
-    perror("recvfrom");
+  addr_len = sizeof their_addr;
+  if((numbytes = recvfrom(sockfd, filename, MAXBUFSZM-1, 0,
+                          (struct sockaddr *)&their_addr, &addr_len)) == -1) {
+    fprintf(stderr, "recvfrom() failed\n");
     exit(1);
   }
 
-  // Communication went well, reset timeout
-  if (setitimer(ITIMER_REAL, NULL, NULL) == -1) {
-    perror("sender: error calling setitimer()");
-    exit(1);
-  }
+  printf("sender: got packet from %s\n",
+         inet_ntop(their_addr.ss_family,
+                   get_in_addr((struct sockaddr *)&their_addr),
+                   s, sizeof s));
+  printf("sender: packet is %d bytes long\n", numbytes);
+  filename[numbytes] = '\0';
+  printf("sender: packet contains '%s'\n", filename);
 
-  printf("sender: End of transmission ACK received\n");
-  printf("sender: Transfer was successful\n");
-  printf("sender: Tearing down the server\n");
-  printf("sender: Good night\n");
-
-  // Closing sockets are always a good practice
   close(sockfd);
-  freeaddrinfo(servinfo);
-
-  // Don't forget to free the strings!
-  free(sendstr);
-  free(buffer);
 
   return 0;
 }
