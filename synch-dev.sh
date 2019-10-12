4405982f10d26502920a9a2cc88d3e51df35765a
Pedro Abreu <abreu223@hotmail.com>
2019-10-12 15:59:54 -0400

    Update synch-dev.sh

--- synch-dev.sh
+++ synch-dev.sh
@@ -2,11 +2,11 @@
 
 # Pass as argument which server you want to connect this to
 
-umount $(pwd)/dev
+# umount $(pwd)/dev
 # If unmount does not work try the following:
-# diskutil unmount force $(pwd)/dev
+diskutil unmount force $(pwd)/dev
 
 rm -rf dev
 mkdir dev
 
-sshfs pdacost@$1.cs.purdue.edu:/homes/pdacost/my_gits/cs546/dev dev
+sshfs pdacost@$1.cs.purdue.edu:/homes/pdacost/gitprojects/cs546/dev dev
