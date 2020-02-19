86f82b5445fa0e6a21b159608504c5ae4997b68c
Pedro Abreu <abreu223@hotmail.com>
2020-02-19 08:16:59 -0500

    Implement get_content

---
+++ spring20/labs/1/webserver.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+import socket
+import sys
+import os
+import re
+
+def get_content(msg):
+    lines = re.split("\r\n\r\n", msg)
+
+    body = lines[1]
+    headers = re.split("\r\n", lines[0])
+
+    headers.append("body: " + lines[1])
+
+    content_dict = {re.split(":", l)[0].strip():re.split(":", l)[1].strip() for l in headers[1:] if l != ""}
+
+    return headers[0], content_dict
+
+serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+hostname = socket.gethostname()
+serversocket.bind((hostname, int(sys.argv[1])))
+serversocket.listen(5)
+
+while(1):
+    print "listening at", hostname, "..."
+    (client, address) = serversocket.accept()
+    child_pid = os.fork()
+
+    if child_pid == 0:
+        print "got a connection from", address
+        msg = client.recv(4096)
+        print "Received ", get_content(msg)
+        break;
+
+    else:
+        continue;
