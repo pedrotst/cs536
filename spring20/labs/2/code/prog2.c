283d1f69412598a450817ddac19289eaeec2918b
Pedro Abreu <abreu223@hotmail.com>
2020-03-29 17:03:11 -0400

    Not sure

--- spring20/labs/2/code/prog2.c
+++ spring20/labs/2/code/prog2.c
@@ -307,8 +307,8 @@ int A_timerinterrupt() {
   printf("\n-------------- A timeout --------------\n");
   printf("The packet was lost, resending #%d\n", current_timer_seqnum);
 
-  erase_timer(current_timer_seqnum);
   send_packet(current_timer_seqnum);
+  pop_timer();
 
   return 0;
 }
