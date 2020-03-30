bf1017fc48140a6f3694b5c5030665eb81e6fa76
Pedro Abreu <abreu223@hotmail.com>
2020-03-29 21:32:38 -0400

    Output SR

---
+++ spring20/labs/2/docs/.auctex-auto/output_gbn.el
@@ -0,0 +1,30 @@
+(TeX-add-style-hook
+ "output_gbn"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("scrartcl" "paper=a4" "fontsize=11pt")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("fontenc" "T1") ("babel" "english") ("microtype" "protrusion=true" "expansion=true") ("graphicx" "pdftex") ("geometry" "margin=0.5in")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "scrartcl"
+    "scrartcl10"
+    "fontenc"
+    "fourier"
+    "babel"
+    "microtype"
+    "amsmath"
+    "amsfonts"
+    "amsthm"
+    "graphicx"
+    "url"
+    "titlesec"
+    "mathtools"
+    "listings"
+    "xcolor"
+    "fancyhdr"
+    "geometry")
+   (TeX-add-symbols
+    '("horrule" 1)))
+ :latex)
+
